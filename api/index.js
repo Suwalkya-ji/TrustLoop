5e254853049cc46ec27b2b81317a54f2227d8a68
dinesh <dineshsuwalkya31@gmail.com>
2026-07-26 14:23:17 +0530

    Fix Vercel serverless function dependencies and DB initialization

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
 const app = require('../server/app');
+const connectDB = require('../server/config/db');
+
+// Initialize database connection for Vercel serverless function invocation
+connectDB();
 
 module.exports = app;
